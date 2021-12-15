6780f8d33b8d473c59daf91fb33575b2236f988d
JamesDean6969 <75815326+JamesDean6969@users.noreply.github.com>
2021-12-15 13:58:08 -0600

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,3 @@
+AddEventHandler("playerspawned", function(spawn)
+    TriggerEvent("chatmessage", "" {0.0.0}, "Hello, Welcome To SASRP. Make Sure To Join The Discord!!! disc.sastateroleplay")
+(end)
