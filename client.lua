38f132a168d3189606bf50b852f85d57e5d4da21
Defect.Dev <75815326+Defectis@users.noreply.github.com>
2021-12-16 10:35:31 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,3 @@
 AddEventHandler("playerspawned", function(spawn)
-    TriggerEvent("chatmessage", "" {0.0.0}, "Hello, Welcome To SASRP. Make Sure To Join The Discord!!! disc.sastateroleplay")
+    TriggerEvent("chatmessage", "" {0.0.0}, "Put Your Message Here")
 (end)
